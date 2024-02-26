e74cef36b527520bef0807724327a53e4de1e3a4
ali <77683515+mazo-ali@users.noreply.github.com>
2024-02-26 18:16:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 # two decorators, same function
 @app.route('/')
 def index():
-    return render_template("microphone.html")    
+    return render_template("home.html")    
     return """
     <h1> Welecome </h1>
     <input type="text" > <br>
     <input type="file" >
     <button> Translate </button>
     """
+@app.route('/')
+def mic():
+    return render_template("microphone.html") 
 
 if __name__ == '__main__':
     app.run(debug=True)
